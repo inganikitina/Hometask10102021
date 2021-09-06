4633d0270b23f7888da33572deb2bf7c0eb8916f
inganikitina <inganikitina9@gmail.com>
2021-09-06 20:31:05 +0300

    commit2

--- src/test/java/Tests/MyFirstTest.java
+++ src/test/java/Tests/MyFirstTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MyFirstTest {
@@ -19,8 +20,27 @@ public class MyFirstTest {
         for (WebElement item : list1
         ) {
             System.out.println(item.getText());
+        }}
+        @Test
+     public void secondTest(){
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            List<String>list2=new ArrayList<>();
+            list2.add("https://www.youtube.com/watch?app=desktop&v=N8Rr7rVf1RA");
+            list2.add("https://chromedriver.chromium.org/downloads");
+            list2.add("https://www.google.by/");
+            list2.add("https://www.instagram.com/?hl=ru");
+            list2.add("https://www.facebook.com/");
+         for (int i=0;i<5;i++){driver.get(list2.get(i));
+
+         }
+
         }
+
+
+
     }
-}
+
 
 
